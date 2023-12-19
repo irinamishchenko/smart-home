19235eba52227bdec8f0325221bbca4d470e364c
Irina Mishchenko <i.mishchenko95@gmail.com>
2023-12-19 21:20:49 +0200

    added settings for some other devices

--- script/settings.js
+++ script/settings.js
@@ -5,9 +5,11 @@ const switcher = document.querySelector(".switcher");
 const switcherBackground = document.querySelector(".switcher-background");
 const switcherButton = document.querySelector(".switcher-button");
 const main = document.querySelector(".settings__main");
+
 // let lampColorsControls;
 
 switcher.addEventListener("click", switchDevicePower);
+// let selectedProgramm;
 
 // let chosenDevice;
 
@@ -22,7 +24,7 @@ const rooms = JSON.parse(localStorage.rooms);
 class Device {
   battery = Math.floor(Math.random() * 100) + 1;
   timer;
-  //   functions = [];
+  functions = {};
   constructor(isOn, title, imageUrl) {
     this.isOn = isOn;
     this.title = title;
@@ -42,27 +44,127 @@ class Device {
       }, 3600000);
     }
   }
-  set functions(functionsArray) {
-    this.functions = functionsArray;
+  setFunctions(functions) {
+    this.functions = functions;
   }
 }
 
-const devices = JSON.parse(localStorage.rooms)
-  .find((room) => room.title === localStorage.selectedRoom)
-  .devices.map((device) => {
-    let newDevice;
-    let isOn = device.isOn;
-    if (!isOn) {
-      newDevice = new Device(device.isOn, device.title, device.imageUrl);
-    } else {
-      newDevice = new Device(device.isOn, device.title, device.imageUrl);
-      newDevice.changeBattery();
-    }
-    return newDevice;
-  });
+function setDevices() {
+  devices = JSON.parse(localStorage.rooms)
+    .find((room) => room.title === localStorage.selectedRoom)
+    .devices.map((device) => {
+      let newDevice;
+      let isOn = device.isOn;
+      if (!isOn) {
+        newDevice = new Device(device.isOn, device.title, device.imageUrl);
+      } else {
+        newDevice = new Device(device.isOn, device.title, device.imageUrl);
+        newDevice.changeBattery();
+      }
+      const functions = setDeviceFunctions(newDevice);
+      newDevice.setFunctions(functions);
+      return newDevice;
+    });
+  rooms.find((room) => room.title === localStorage.selectedRoom).devices =
+    devices;
+  localStorage.setItem("rooms", JSON.stringify(rooms));
+}
+
+let devices = [];
+setDevices();
 
 if (devices.length > 1) {
   showDevices();
+}
+
+function setDeviceFunctions(device) {
+  let functions;
+  switch (device.title) {
+    case "air-condition":
+      functions = { temp: 24, modes: ["cool", "hot", "auto"] };
+      break;
+    case "air-purifier":
+      functions = { power: [1, 2, 3] };
+      break;
+    case "cleaner-robot":
+      functions = { power: [1, 2, 3], modes: ["wet", "dry"] };
+      break;
+    case "coffee-machine":
+      functions = {
+        modes: ["Cappuccino", "Cocoa", "Latte", "Espresso", "Black Coffee"],
+      };
+      break;
+    case "fan":
+      functions = { power: [1, 2, 3] };
+      break;
+    case "fridge":
+      functions = { power: [1, 2, 3] };
+      break;
+    case "griller":
+      functions = { modes: ["Meat", "Fish", "Veggies", "Shrimps"] };
+      break;
+    case "heater-machine":
+      functions = { power: [1, 2, 3] };
+      break;
+    case "light":
+      functions = { colors: ["blue", "white", "yellow"], brightness: 50 };
+      break;
+    case "loud":
+      functions = { volume: 50 };
+      break;
+    case "microwave-oven":
+      functions = { power: 600, time: 5 };
+      break;
+    case "rice-cooker":
+      functions = {
+        modes: ["Rice", "Soup", "Oatmeal", "Slow cook", "Reheat", "Warm"],
+      };
+      break;
+    case "smart-door":
+      functions = { volume: 50 };
+      break;
+    case "stove":
+      functions = {
+        modes: ["Pizza", "Convection", "Bottom", "Grill", "Fan Grill"],
+        temp: 180,
+        light: false,
+      };
+      break;
+    case "tv":
+      functions = {
+        programms: [
+          "Discovery",
+          "Fox",
+          "ICTV",
+          "Euronews",
+          "MTV",
+          "Eurosport",
+          "FilmBox",
+        ],
+        volume: 50,
+        selectedProgramm: null,
+      };
+      break;
+    case "washing-machine":
+      functions = {
+        modes: [
+          "Hand Wash",
+          "Quick wash",
+          "Sport",
+          "Cotton",
+          "Bio Wash",
+          "Soft Wash",
+        ],
+      };
+      break;
+    case "water-boiler":
+      functions = { temp: 50 };
+      break;
+
+    default:
+      break;
+  }
+  return functions;
 }
 
 function changeTitle() {
@@ -79,7 +181,6 @@ function changeChargePercentage() {
   } %`;
 }
 
-// setChosenDevice();
 changeTitle();
 changeChargePercentage();
 
@@ -119,7 +220,6 @@ function setSelectedDevice(event) {
       );
     }
   }
-  //   setChosenDevice();
   changeSwitcherStyles();
   changeTitle();
   changeChargePercentage();
@@ -166,6 +266,12 @@ function changeSwitcherStyles() {
 function setDeviceSettingsMarkup() {
   const container = document.querySelector(".main__device-settings");
   let deviceSettingsMarkup;
+  const device = rooms
+    .find((room) => room.title === localStorage.selectedRoom)
+    .devices.find((device) => device.title === localStorage.selectedDevice);
+  console.log(device);
+  let { title, functions } = device;
+  console.log(title, functions);
   switch (localStorage.selectedDevice) {
     case "light":
       deviceSettingsMarkup = `<div class="lamp"><div class="lamp__img-container"><img class="lamp-img" src="../images/settings/lamp.png" /><svg class="lamp-icon"><use xlink:href="../images/sprite.svg#lamp-light"></use></svg></div><div class="lamp__settings"><section class="lamp__settings__colors"><h3 class="lamp__settings__colors-title">Colors</h3><div class="lamp__settings__colors__buttons"><button class="lamp__settings__colors__button button-blue" data-color="#254bec"></button><button class="lamp__settings__colors__button button-white" data-color="#fff"></button><button class="lamp__settings__colors__button button-yellow" data-color="#ffc600"></button></div></section><section class="lamp__settings__brightness"><h3 class="lamp__settings__brightness-title">Brightness</h3><div class="lamp__settings__brightness-controls"><button class="lamp__settings__brightness-less"><svg class="controls-icon"><use xlink:href="../images/sprite.svg#minus"></use></svg></button><p class="lamp__settings__brightness-info">50%</p><button class="lamp__settings__brightness-more"><svg class="controls-icon"><use xlink:href="../images/sprite.svg#plus"></use></svg></button></div></section></div></div>`;
@@ -195,8 +301,65 @@ function setDeviceSettingsMarkup() {
         .forEach((mode) => mode.addEventListener("click", setConditionMode));
       setConditionMode();
       break;
+    case "coffee-machine":
+      deviceSettingsMarkup = `<div class="coffee"><img class="coffee-img" src="./../images/settings/coffee-machine.png"/><ul class="coffee-drinks"><li class="coffee-drink" data-coffee="cappuccino"><div class="coffee-drink-img-wrapper"><img class="coffee-drink-img" src="./../images/settings/cappuccino.png" alt="cappuccino" /></div><h3 class="coffee-drink-title">Cappuccino</h3><button class="coffee-drink-button">Start</button></li><li class="coffee-drink" data-coffee="cocoa"><div class="coffee-drink-img-wrapper"><img class="coffee-drink-img" src="./../images/settings/cocoa.png" alt="cocoa" /></div><h3 class="coffee-drink-title">Cocoa</h3><button class="coffee-drink-button">Start</button></li><li class="coffee-drink" data-coffee="latte"><div class="coffee-drink-img-wrapper"><img class="coffee-drink-img" src="./../images/settings/latte.png" alt="latte" /></div><h3 class="coffee-drink-title">Latte</h3><button class="coffee-drink-button">Start</button></li><li class="coffee-drink" data-coffee="espresso"><div class="coffee-drink-img-wrapper"><img class="coffee-drink-img" src="./../images/settings/espresso.png" alt="espresso" /></div><h3 class="coffee-drink-title">Espresso</h3><button class="coffee-drink-button">Start</button></li><li class="coffee-drink" data-coffee="coffee"><div class="coffee-drink-img-wrapper"><img class="coffee-drink-img" src="./../images/settings/coffee.png" alt="coffee" /></div><h3 class="coffee-drink-title">Black Coffee</h3><button class="coffee-drink-button">Start</button></li></ul></div>`;
+      container.innerHTML = deviceSettingsMarkup;
+      document
+        .querySelectorAll(".coffee-drink-button")
+        .forEach((button) => button.addEventListener("click", makeCoffee));
+      break;
+    case "tv":
+      let programmsItems = ``;
+      for (let i = 0; i < functions.programms.length; i++) {
+        programmsItems += `<li class="tv__programms__list-item" data-channel="${functions.programms[i]}">${functions.programms[i]}</li>`;
+      }
+      deviceSettingsMarkup = `<div class="tv"><div class="tv__programms"><h3 class="tv__programms-title">Programms</h3><ul class="tv__programms__list">${programmsItems}</ul></div><div class="tv__settings"><div class="tv__settings__volume"><h3 class="tv__settings__volume-title">Volume</h3><div class="tv__settings__volume-bar"><button class="tv__settings__volume-bar-button" data-volume="less"><svg class="tv__settings__volume-bar-icon"><use xlink:href="./../images/sprite.svg#minus"></use></svg></button><p class="tv__settings__volume-bar-value">${functions.volume} %</p><button class="tv__settings__volume-bar-button" data-volume="more"><svg class="tv__settings__volume-bar-icon"><use xlink:href="./../images/sprite.svg#plus"></use></svg></button></div></div><div class="tv__settings__controls"><h3 class="tv__settings__controls-title">Controls</h3><div class="tv__settings__controls-container"><button class="tv__settings__controls-button control-prev" data-programm="prev"><svg class="tv__settings__controls-icon"><use xlink:href="./../images/sprite.svg#arrow"></use></svg></button><button class="tv__settings__controls-button control-next" data-programm="next"><svg class="tv__settings__controls-icon"><use xlink:href="./../images/sprite.svg#arrow"></use></svg></button></div></div></div></div>`;
+      container.innerHTML = deviceSettingsMarkup;
+      document
+        .querySelectorAll(".tv__programms__list-item")
+        .forEach((programm) =>
+          programm.addEventListener("click", setSelectedProrgamm)
+        );
+      document
+        .querySelectorAll(".tv__settings__volume-bar-button")
+        .forEach((button) => button.addEventListener("click", changeTvVolume));
+      setSelectedProrgamm();
+      document
+        .querySelectorAll(".tv__settings__controls-button")
+        .forEach((button) =>
+          button.addEventListener("click", setSelectedProrgamm)
+        );
+      break;
+    case "griller":
+      let modesItems = ``;
+      for (let i = 0; i < functions.modes.length; i++) {
+        modesItems += `<li class="mode-item" data-mode="${functions.modes[
+          i
+        ].toLowerCase()}"><div class="mode-item__img-wrapper"><img class="mode-item__img" src="./../images/settings/${functions.modes[
+          i
+        ].toLowerCase()}.png"/></div><h3 class="mode-item-title">${
+          functions.modes[i]
+        }</h3></li>`;
+      }
+      deviceSettingsMarkup = `<div class="griller"><ul class="mode-list">${modesItems}</ul></div>`;
+      container.innerHTML = deviceSettingsMarkup;
+      document
+        .querySelectorAll(".mode-item")
+        .forEach((mode) => mode.addEventListener("click", changeMode));
+      break;
 
     default:
+      let powerItems = ``;
+      functions.power.forEach(
+        (item) =>
+          (powerItems += `<li class="default-device__power">${item}</li>`)
+      );
+      deviceSettingsMarkup = `<div class="default-device"><img class="default-device__img" src="./../images/settings/${title}.png" alt="${title}" /><div class="default-device__wrapper"><h3 class="default-device-subtitle">Power</h3><ul class="default-device__powers">${powerItems}</ul></div></div>`;
+      container.innerHTML = deviceSettingsMarkup;
+      document
+        .querySelectorAll(".default-device__power")
+        .forEach((item) => item.addEventListener("click", setPowerDefault));
+      setPowerDefault();
       break;
   }
 }
@@ -206,7 +369,6 @@ setDeviceSettingsMarkup();
 function setSelectedLampColor(event) {
   let selectedColor;
   const buttons = document.querySelectorAll(".lamp__settings__colors__button");
-
   if (!event) {
     selectedColor = "#fff";
   } else {
@@ -275,6 +437,103 @@ function setConditionMode(event) {
       modes[i].classList.add("condition-mode--selected");
     } else {
       modes[i].classList.remove("condition-mode--selected");
+    }
+  }
+}
+
+function makeCoffee(event) {
+  switchDevicePower();
+  event.target.textContent = "In progress";
+  setTimeout(() => {
+    alert("Your drink is done!");
+    event.target.textContent = "Start";
+    switchDevicePower();
+  }, 120000);
+}
+
+function setPowerDefault(event) {
+  let selectedPower;
+  const items = document.querySelectorAll(".default-device__power");
+  if (!event) {
+    selectedPower = 1;
+  } else {
+    selectedPower = +event.target.textContent;
+  }
+  for (let i = 0; i < items.length; i++) {
+    if (+items[i].textContent === selectedPower) {
+      items[i].classList.add("default-device__power--selected");
+    } else {
+      items[i].classList.remove("default-device__power--selected");
+    }
+  }
+}
+
+function setSelectedProrgamm(event) {
+  const channels = document.querySelectorAll(".tv__programms__list-item");
+  const programms = devices.find(
+    (device) => device.title === localStorage.selectedDevice
+  ).functions.programms;
+  if (!event) {
+    selectedProgramm = programms[0];
+  } else if (event.target.dataset.channel) {
+    selectedProgramm = event.target.dataset.channel;
+  } else {
+    let index = programms.indexOf(selectedProgramm);
+    if (event.currentTarget.dataset.programm === "next") {
+      if (index >= programms.length - 1) {
+        index = -1;
+      }
+      selectedProgramm = programms[index + 1];
+    } else {
+      if (index < 1) {
+        index = programms.length;
+      }
+      selectedProgramm = programms[index - 1];
+    }
+  }
+  for (let i = 0; i < channels.length; i++) {
+    if (channels[i].dataset.channel === selectedProgramm) {
+      channels[i].classList.add("tv__programms__list-item--selected");
+    } else {
+      channels[i].classList.remove("tv__programms__list-item--selected");
+    }
+  }
+}
+
+function changeTvVolume(event) {
+  let volumeEl = document.querySelector(".tv__settings__volume-bar-value");
+  let volumeValue = parseInt(volumeEl.textContent);
+  const lessButton = document.querySelector(
+    ".tv__settings__volume-bar-button[data-volume='less'"
+  );
+  const moreButton = document.querySelector(
+    ".tv__settings__volume-bar-button[data-volume='more'"
+  );
+  if (volumeValue === 99) {
+    moreButton.disabled = true;
+  } else if (volumeValue === 1) {
+    lessButton.disabled = true;
+  } else {
+    moreButton.disabled = false;
+    lessButton.disabled = false;
+  }
+  if (event.currentTarget.dataset.volume === "more") {
+    volumeValue += 1;
+  } else {
+    volumeValue -= 1;
+  }
+  volumeEl.textContent = volumeValue + "%";
+}
+
+function changeMode(event) {
+  let selectedMode;
+  const modeElements = document.querySelectorAll(".mode-item");
+  selectedMode = event.currentTarget.dataset.mode;
+  for (let i = 0; i < modeElements.length; i++) {
+    if (modeElements[i].dataset.mode === selectedMode) {
+      modeElements[i].classList.add("mode-item--selected");
+    } else {
+      modeElements[i].classList.remove("mode-item--selected");
     }
   }
 }
